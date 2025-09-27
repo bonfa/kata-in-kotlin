4bca46d142eb7cc24aaf1325e21e1c72ac0bcabb
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-09-27 12:30:21 +0200

    [BOWLING] make BowlingGame more clear

--- blank-maven-project/src/main/kotlin/it/fbonfadelli/playground/bowling/BowlingGame.kt
+++ blank-maven-project/src/main/kotlin/it/fbonfadelli/playground/bowling/BowlingGame.kt
@@ -2,17 +2,17 @@ package it.fbonfadelli.playground.bowling
 
 object BowlingGame {
 
-    fun totalScore(frames: List<Frame>): Int {
-        var totalScore = 0
-        for (i in 0 until frames.size) {
-            totalScore += when (val frame = frames[i]) {
-                is LastFrame -> frame.getScore()
-                is NorStrikeNorSpare -> frame.getScore()
-                is Spare -> frame.getScore(frames[i + 1])
-                is Strike -> frame.getScore(frames[i + 1], frames.getOrNull(i + 2))
-            }
-        }
-        return totalScore
-    }
+    fun totalScore(frames: List<Frame>): Int =
+        frames.mapIndexed { i, frame -> frameScore(frame, frames, i) }.sum()
 
+    private fun frameScore(
+        frame: Frame,
+        frames: List<Frame>,
+        framePosition: Int
+    ): Int = when (frame) {
+        is LastFrame -> frame.getScore()
+        is NorStrikeNorSpare -> frame.getScore()
+        is Spare -> frame.getScore(frames[framePosition + 1])
+        is Strike -> frame.getScore(frames[framePosition + 1], frames.getOrNull(framePosition + 2))
+    }
 }
