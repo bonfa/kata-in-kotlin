63822b26b431f504bc0e6133163383b98f6f458e
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-09-27 18:40:15 +0200

    [CODE_CRACKER] formatting

--- src/main/kotlin/it/fbonfadelli/playground/codecracker/Decryptor.kt
+++ src/main/kotlin/it/fbonfadelli/playground/codecracker/Decryptor.kt
@@ -1,6 +1,7 @@
 package it.fbonfadelli.playground.codecracker
 
 class Decryptor(private val key: String) {
+
     fun decrypt(message: String): String =
         message
             .map { char -> ('a'.code + key.indexOf(char)).toChar() }
--- src/main/kotlin/it/fbonfadelli/playground/codecracker/Encryptor.kt
+++ src/main/kotlin/it/fbonfadelli/playground/codecracker/Encryptor.kt
@@ -1,6 +1,9 @@
 package it.fbonfadelli.playground.codecracker
 
 class Encryptor(private val key: String) {
+
     fun encrypt(message: String): String =
-        message.map { key[it - 'a'] }.joinToString("")
+        message
+            .map { key[it - 'a'] }
+            .joinToString("")
 }
