7fa4187e06f8822cfe41ad879182475e23d0fec2
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-09-27 18:26:29 +0200

    [CODE_CRACKER] now we have also an acceptance test

---
+++ src/test/kotlin/it/fbonfadelli/playground/codecracker/CodeCrackerAcceptanceTest.kt
@@ -0,0 +1,31 @@
+package it.fbonfadelli.playground.codecracker
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CodeCrackerAcceptanceTest {
+
+    private val encryptor = Encryptor()
+    private val decryptor = Decryptor()
+
+
+    @Test
+    fun `a message`() {
+        val key = "bcdefghijklmnopqrstuvwxyza"
+        val message = "abbbabccb"
+
+        val decrypted = decryptor.decrypt(encryptor.encrypt(message, key), key)
+
+        assertThat(decrypted).isEqualTo(message)
+    }
+
+    @Test
+    fun `all the alphabet`() {
+        val key = "bcdefghijklmnopqrstuvwxyza"
+        val message = "abcdefghijklmnopqrstuvwxyz"
+
+        val decrypted = decryptor.decrypt(encryptor.encrypt(message, key), key)
+
+        assertThat(decrypted).isEqualTo(message)
+    }
+}
