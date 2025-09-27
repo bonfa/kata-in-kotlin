ec84f5f449b972252020e8029c799b0be1101b89
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-09-27 18:00:00 +0200

    [CODE_CRACKER] remove test list for encryptor

--- src/test/kotlin/it/fbonfadelli/playground/codecracker/CodeCrackerTest.kt
+++ src/test/kotlin/it/fbonfadelli/playground/codecracker/CodeCrackerTest.kt
@@ -8,13 +8,6 @@ class CodeCrackerTest {
 
     /*
 
-    encryptor - (message, key) --> encrypted message
-        - "", any() -> ""
-        - "a", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "b"
-        - "b", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "c"
-        - "ab", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "bc"
-        - "abbbabccb", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "bcccbcddc"
-
     decryptor - (message, key) --> decrypted message
         - "", any() -> ""
         - "b", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "a"
