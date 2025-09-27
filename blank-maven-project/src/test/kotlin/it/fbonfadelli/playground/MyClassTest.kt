cf330e9beaafabba2669a2bb989e0cc5f12e77df
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-09-27 12:15:57 +0200

    [BOWLING] clean up a bit the frame creator

--- blank-maven-project/src/test/kotlin/it/fbonfadelli/playground/MyClassTest.kt
+++ blank-maven-project/src/test/kotlin/it/fbonfadelli/playground/MyClassTest.kt
@@ -194,23 +194,28 @@ class Bowling(private val rollScores: List<Int>) {
 
         var i = 0
         var prevScore: Int? = null
-        while (prevScore != null || i < rollScores.size - 3)  {
+        while (prevScore != null || i < rollScores.size - 3) {
             val rollScore = rollScores[i]
-            if (rollScore == 10) {
-                frames.add(Strike)
-                prevScore = null
-                i++
-            } else if (prevScore == null) {
-                prevScore = rollScore
-                i++
-            } else {
-                if (prevScore + rollScore == 10) {
-                    frames.add(Spare(prevScore))
-                } else {
-                    frames.add(NorStrikeNorSpare(prevScore, rollScore))
+            when {
+                rollScore == 10 -> {
+                    frames.add(Strike)
+                    prevScore = null
+                    i++
                 }
-                prevScore = null
-                i++
+                prevScore == null -> {
+                    prevScore = rollScore
+                    i++
+                }
+                prevScore + rollScore == 10 -> {
+                    frames.add(Spare(prevScore))
+                    prevScore = null
+                    i++
+                }
+                else -> {
+                    frames.add(NorStrikeNorSpare(prevScore, rollScore))
+                    prevScore = null
+                    i++
+                }
             }
         }
 
