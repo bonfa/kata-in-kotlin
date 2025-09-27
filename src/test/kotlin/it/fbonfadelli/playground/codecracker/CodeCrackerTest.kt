6a1cd4dcca9c20edefd5a12a2b5b3f44fbfe8fc6
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-09-27 17:59:36 +0200

    [CODE_CRACKER] now we completed our encryptor

--- src/test/kotlin/it/fbonfadelli/playground/codecracker/CodeCrackerTest.kt
+++ src/test/kotlin/it/fbonfadelli/playground/codecracker/CodeCrackerTest.kt
@@ -13,20 +13,20 @@ class CodeCrackerTest {
         - "a", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "b"
         - "b", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "c"
         - "ab", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "bc"
-        - "abbba bccb", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "bcccb cddc"
+        - "abbbabccb", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "bcccbcddc"
 
     decryptor - (message, key) --> decrypted message
         - "", any() -> ""
         - "b", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "a"
         - "c", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "b"
         - "bc", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "ab"
-        - "bcccb cddc", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "abbba bccb"
+        - "bcccbcddc", "b c d e f g h i j k l m n o p q r s t u v w x y z a" -> "abbbabccb"
 
     acceptance
         - "a", any() -> [encrypt-decrypt] -> "a"
         - "b", any() -> [encrypt-decrypt] -> "b"
         - "ab", any() -> [encrypt-decrypt] -> "ab"
-        - "abbba bccb", any() -> [encrypt-decrypt] -> "abbba bccb"
+        - "abbbabccb", any() -> [encrypt-decrypt] -> "abbbabccb"
      */
 
     @Test
@@ -56,16 +56,23 @@ class CodeCrackerTest {
         val key = "cdefghijklmnopqrstuvwxyzab"
         assertThat(encryptor.encrypt("a", key)).isEqualTo("c")
     }
+
+    @Test
+    fun `encryptor - 2 characters`() {
+        val encryptor = Encryptor()
+        val key = "bcdefghijklmnopqrstuvwxyza"
+        assertThat(encryptor.encrypt("aa", key)).isEqualTo("bb")
+    }
+
+    @Test
+    fun `encryptor - many characters`() {
+        val encryptor = Encryptor()
+        val key = "bcdefghijklmnopqrstuvwxyza"
+        assertThat(encryptor.encrypt("abbbabccb", key)).isEqualTo("bcccbcddc")
+    }
 }
 
 class Encryptor {
-    fun encrypt(message: String, key: String): String {
-        key.toCharArray()
-
-        return when {
-            message.isEmpty() -> ""
-            else -> key[message.first() - 'a'].toString()
-        }
-    }
-
+    fun encrypt(message: String, key: String): String =
+        message.map { key[it - 'a'] }.joinToString("")
 }
