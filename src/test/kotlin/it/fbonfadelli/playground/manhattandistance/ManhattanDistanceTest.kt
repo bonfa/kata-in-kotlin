08d5bd9a90da55d23104d9314c434ab8613b86e7
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-10-03 12:27:55 +0200

    [MANHATTAN_DISTANCE] remove test list

--- src/test/kotlin/it/fbonfadelli/playground/manhattandistance/ManhattanDistanceTest.kt
+++ src/test/kotlin/it/fbonfadelli/playground/manhattandistance/ManhattanDistanceTest.kt
@@ -6,25 +6,6 @@ import kotlin.math.abs
 
 class ManhattanDistanceTest {
 
-    /*
-
-    CONSTRAINTS
-    The class Point is immutable
-    The class Point has no Getters, no Setters
-    The class Point has no public properties (i.e. the internal state cannot be read from outside the class).
-
-    SUGGESTED TESTS
-    manhattanDistance( Point(1, 1), Point(1, 1) ) should returns 0
-    manhattanDistance( Point(5, 4), Point(3, 2) ) should returns 4
-    manhattanDistance( Point(1, 1), Point(0, 3) ) should returns 3
-
-    TEST LIST
-    - same point
-    - changing only one coordinate (y) [x]
-    - changing only other coordinate (x)
-    - changing both coordinates (x, y)
-     */
-
     @Test
     fun `distance between a point and itself is zero`() {
         assertThat(manhattanDistance(Point(1, 1), Point(1, 1))).isEqualTo(0)
