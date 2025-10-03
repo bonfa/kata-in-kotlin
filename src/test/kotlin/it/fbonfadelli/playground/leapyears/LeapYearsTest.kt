c7641d93d3249d0ff761a9508bd3e916a534a1a2
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-10-03 11:44:49 +0200

    [LEAP_YEARS] remove test list

--- src/test/kotlin/it/fbonfadelli/playground/leapyears/LeapYearsTest.kt
+++ src/test/kotlin/it/fbonfadelli/playground/leapyears/LeapYearsTest.kt
@@ -7,16 +7,6 @@ import java.time.Year
 
 class LeapYearsTest {
 
-    /*
-    All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
-    All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
-    All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
-    All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
-
-    - 2000 --> leap year
-
-     */
-
     @Test
     fun `odd year`() {
         assertThat(isLeapYear(2017)).isFalse
