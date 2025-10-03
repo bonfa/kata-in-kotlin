448251ff44456b51ef4ed29554f77274653095b1
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-10-03 11:37:15 +0200

    [LEAP_YEARS] now we are able to say that ood years are not leap years

---
+++ src/test/kotlin/it/fbonfadelli/playground/leapyears/LeapYearsTest.kt
@@ -0,0 +1,31 @@
+package it.fbonfadelli.playground.leapyears
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import java.time.Year
+
+class LeapYearsTest {
+
+    /*
+    All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+    All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+    All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
+    All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
+
+    - 2018, 2017 --> not leap year
+    - 2008 --> leap year
+    - 1700 --> not leap year
+    - 2000 --> leap year
+
+     */
+
+    @Test
+    fun `odd year`() {
+        assertThat(isLeapYear(2017)).isFalse()
+    }
+
+    private fun isLeapYear(year: Int): Boolean {
+        return false
+    }
+}
