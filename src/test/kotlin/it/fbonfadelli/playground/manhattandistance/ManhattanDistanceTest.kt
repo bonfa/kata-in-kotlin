6037ba149cd88a1ea67967aa9dbc00e43749bc30
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-10-03 12:22:43 +0200

    [MANHATTAN_DISTANCE] now the manhattan distance is able to work with points with same x

--- src/test/kotlin/it/fbonfadelli/playground/manhattandistance/ManhattanDistanceTest.kt
+++ src/test/kotlin/it/fbonfadelli/playground/manhattandistance/ManhattanDistanceTest.kt
@@ -3,6 +3,7 @@ package it.fbonfadelli.playground.manhattandistance
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
 
 class ManhattanDistanceTest {
 
@@ -20,7 +21,7 @@ class ManhattanDistanceTest {
 
     TEST LIST
     - same point
-    - changing only one coordinate (y)
+    - changing only one coordinate (y) [x]
     - changing only other coordinate (x)
     - changing both coordinates (x, y)
      */
@@ -30,12 +31,26 @@ class ManhattanDistanceTest {
         assertThat(manhattanDistance(Point(1, 1), Point(1, 1))).isEqualTo(0)
     }
 
+    @Test
+    fun `points with same x - y of first point is smaller than the y of second point`() {
+        assertThat(manhattanDistance(Point(1, 1), Point(1, 2))).isEqualTo(1)
+    }
+
+    @Test
+    fun `points with same x - y of first point is greater than the y of second point`() {
+        assertThat(manhattanDistance(Point(1, 4), Point(1, 2))).isEqualTo(2)
+    }
+
     private fun manhattanDistance(
         point: Point,
         point2: Point
     ): Int {
-        return 0
+        return point.manhattanDistanceFrom(point2)
     }
 
-    data class Point(private val x: Int, private val y: Int)
+    data class Point(private val x: Int, private val y: Int) {
+        fun manhattanDistanceFrom(point2: Point): Int {
+            return abs(point2.y - this.y)
+        }
+    }
 }
