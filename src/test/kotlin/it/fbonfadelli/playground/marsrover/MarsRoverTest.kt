0107a55950cf5307cca7ee434d4c3eb4b9fd5899
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-10-04 12:40:45 +0200

    [MARS_ROVER] write down axes of complexity and some initial tests

--- src/test/kotlin/it/fbonfadelli/playground/marsrover/MarsRoverTest.kt
+++ src/test/kotlin/it/fbonfadelli/playground/marsrover/MarsRoverTest.kt
@@ -6,6 +6,25 @@ import org.junit.jupiter.api.Test
 
 class MarsRoverTest {
 
+    /*
+    DIMENSIONS
+     - space direction (1D, 2D)
+     - facing / no facing
+     - commands (forward, turn right, turn left)
+        - 1 command at the time
+     - 1 / more commands
+     - obstacles / no obstacles
+
+     TESTS
+     - map: 1D, 1 command (forward)
+     - map: 1D, 1 command (turn right)
+     - map: 1D, 1 command (turn left)
+     - map: 1D, 2 commands (forward, forward)
+     - map: 1D, 2 commands (forward, turn right)
+     - map: 1D, 2 commands (forward, turn left)
+     - map: 2D, 1 command (turn right)
+     */
+
     @Test
     fun `my first test`() {
         assertThat(true).isTrue
