a60c5898c944e96b965ae59d3681d53c31043d93
Francesco Bonfadelli <francesco.bonfadelli@gmail.com>
2025-10-04 12:30:00 +0200

    [MARS_ROVER] prepare test class

---
+++ src/test/kotlin/it/fbonfadelli/playground/marsrover/MarsRoverTest.kt
@@ -0,0 +1,13 @@
+package it.fbonfadelli.playground.marsrover
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class MarsRoverTest {
+
+    @Test
+    fun `my first test`() {
+        assertThat(true).isTrue
+    }
+}
